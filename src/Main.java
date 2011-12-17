55478938b28542d2564bbe1fa980d4cfbf269c99
Sergey Ivaschuck <kwyjibo1988@gmail.com>
2011-12-17 20:47:43 +0000

    second commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import com.kwyjibo.common.FileScanner;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FileScanner f = new FileScanner();
+		f.test();
+	}
+
+}
---
+++ src/com/kwyjibo/common/FileScanner.java
@@ -0,0 +1,12 @@
+package com.kwyjibo.common;
+
+public class FileScanner {
+
+	public FileScanner(){
+		
+	}
+	
+	public void test(){
+		System.out.println("FileScanner: active");
+	}
+}
