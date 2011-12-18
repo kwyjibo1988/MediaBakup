214959042bf103ed0d42fce8dad7053bb840b819
Sergey Ivaschuck <kwyjibo1988@gmail.com>
2011-12-18 14:49:40 +0000

    tested the database

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,12 @@
 import com.kwyjibo.common.FileScanner;
-import com.mongodb.Mongo;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
+import com.kwyjibo.database.DataStore;
 
 public class Main {
 
 	public static void main(String[] args) {
 		FileScanner f = new FileScanner();
 		f.test();
+		DataStore ds = new DataStore();
 	}
 
 }
--- src/com/kwyjibo/database/DataStore.java
+++ src/com/kwyjibo/database/DataStore.java
@@ -1,9 +1,54 @@
 package com.kwyjibo.database;
 
-public class DataStore {
+import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+public class DataStore {
+	private DB db;
+	
 	public DataStore() {
-		
+		try {
+			connect();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		insertObject();
+	}
+	
+	private void connect() throws UnknownHostException, MongoException{
+		Mongo m = new Mongo();
+		db = m.getDB("mydb");
+	}
+	
+	private void insertObject(){
+		DBCollection coll = db.getCollection("testCollection");
+		BasicDBObject doc = new BasicDBObject();
+
+        doc.put("name", "MongoDB");
+        doc.put("type", "database");
+        doc.put("count", 1);
+
+        BasicDBObject info = new BasicDBObject();
+
+        info.put("x", 203);
+        info.put("y", 102);
+
+        doc.put("info", info);
+
+        coll.insert(doc);
+        
+        DBObject myDoc = coll.findOne();
+        System.out.println(myDoc);
 	}
 
 }
