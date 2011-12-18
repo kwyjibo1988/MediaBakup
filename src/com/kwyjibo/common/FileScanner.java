5f21007a1becb594a4f44fc8de926899132daf27
Sergey Ivaschuck <kwyjibo1988@gmail.com>
2011-12-18 16:35:21 +0000

    local changes

--- src/com/kwyjibo/common/FileScanner.java
+++ src/com/kwyjibo/common/FileScanner.java
@@ -2,6 +2,15 @@ package com.kwyjibo.common;
 import java.io.File;
 import java.io.IOException;
 
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
 public class FileScanner implements FileScannerImpl {
 
 	public FileScanner(String args){
